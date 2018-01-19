7076bd8a4d82259229dde4b654981bdd0b8fc4dd
Suresh Subramaniam <smashthenuts@gmail.com>
2018-01-19 12:39:20 -0800

    Cleaned additional object example

--- jsbase.js
+++ jsbase.js
@@ -10,16 +10,3 @@ var suresh = {
         alert('Hello, I\'m ' + this.name[0] + ' ' + this.name[1] + '.' + 'I am ' + this.age + ' years old and I am married to ' + this.spouse +'. My parents are ' + this.parents[0] + ' and ' + this.parents[1] + '. I have'+ this.kids.length +' kids, ' + this.kids[0] +' and ' + this.kids[1])
     }
 };
-
-var person = {
-    name: ['Bob', 'Smith'],
-    age: 32,
-    gender: 'male',
-    interests: ['music', 'skiing'],
-    bio: function() {
-      alert(this.name[0] + ' ' + this.name[1] + ' is ' + this.age + ' years old. He likes ' + this.interests[0] + ' and ' + this.interests[1] + '.');
-    },
-    greeting: function() {
-      alert('Hi! I\'m ' + this.name[0] + '.');
-    }
-  };
